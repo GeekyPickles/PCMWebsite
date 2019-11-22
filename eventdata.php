16e079cea11237a6dfee19005c940e9552a18409
GeekyPickles <geekypickles@gmail.com>
2019-11-22 14:55:10 -0800

    Add files via upload

---
+++ eventdata.php
@@ -0,0 +1,10 @@
+<?php
+  $events = array("event1", "event2", "event3", "event4", "event6", "event7");
+
+  // event1
+  $events["event1"]["name"] = "General Meeting";
+  $events["event1"]["description"] = "Come meet PCM!";
+  $events["event1"]["time"] = "";
+  $events["event1"]["picture"] = "upcomingevents/event1/event1cover.jpg";
+
+ ?>
